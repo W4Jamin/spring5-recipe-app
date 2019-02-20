39cfb0d19b15aeec3ada3ae23731d77d80004db2
W4Jamin <yinkop@hotmail.com>
2019-02-19 22:40:04 -0500

    Initiate project

---
+++ src/test/java/by9ye/recipe/recipewebapp/RecipeWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package by9ye.recipe.recipewebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeWebAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
