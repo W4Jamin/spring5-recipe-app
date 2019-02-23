15f575f488e4428b384a0f80484c2ad494f6eb34
W4Jamin <yinkop@hotmail.com>
2019-02-23 15:19:49 -0500

    Finish adding project lombok

--- src/main/java/by9ye/recipe/recipewebapp/model/Category.java
+++ src/main/java/by9ye/recipe/recipewebapp/model/Category.java
@@ -1,11 +1,14 @@
 package by9ye.recipe.recipewebapp.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipes"}) // bidirectional relationship will
+// cause stack overflow when generating hash functions
 @Entity
 public class Category {
 
--- src/main/java/by9ye/recipe/recipewebapp/model/Notes.java
+++ src/main/java/by9ye/recipe/recipewebapp/model/Notes.java
@@ -1,10 +1,12 @@
 package by9ye.recipe.recipewebapp.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
